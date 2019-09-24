5d06fb0e2fafbec68cd10827e217f127501c8175
karla <karla.leal1988@gmail.com>
2019-09-24 19:02:48 -0300

    create rate controller

--- db/migrate/20190924165351_create_rate_models.rb
+++ db/migrate/20190924165351_create_rate_models.rb
@@ -2,7 +2,7 @@ class CreateRateModels < ActiveRecord::Migration[6.0]
   def change
     create_table :rate_models do |t|
       t.datetime :date
-      t.float :currency
+      t.string :currency
       t.float :equivalence
 
       t.timestamps
